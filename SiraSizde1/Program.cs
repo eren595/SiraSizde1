76184b15d747f691df171522875138390cefd6d8
eren595 <eren595@BPC12>
2023-12-07 13:37:18 +0300

    Proje dosyası ekle.

---
+++ SiraSizde1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiraSizde1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranIslem ei = new EkranIslem();
+            ei.EkranaYaz(3,4,0,5);
+            Console.WriteLine("=======================");
+            ei.EkranaYaz();
+        }
+    }
+    class EkranIslem
+    {
+        public void EkranaYaz(params int[] sayilar)
+        {
+            if (sayilar.Length == 0)
+            {
+                Console.WriteLine("Parametre olmadığı için metottan çıkılıyor.");
+                return;
+            }
+            Console.WriteLine("Parametreden gelen değerler:");
+            foreach (var s in sayilar)
+            {
+                if (s==0) return;
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+
+
